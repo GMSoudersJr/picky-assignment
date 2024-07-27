90b3ee372dc2db1ae0a6d08c6eb3638859e85375
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-07-28 01:38:09 +0900

    fix(server): deployment
    read Vercel documentation to see if I can fix this api call issue in
    production.

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -4,10 +4,28 @@ export const isSingular = (count: number): boolean => {
   return count === 1;
 }
 
+const dev = process.env.NODE_ENV !== 'production';
+
+const server = dev ? 'http://localhost:3000/api' : '';
+
+export async function getDiscussions(): Promise<TDiscussion[] | undefined> {
+  let result: TDiscussion[] = [];
+  try {
+    const res = await fetch(`${server}/discussions`);
+    if (!res.ok) return result;
+
+    result = await res.json();
+  } catch (error) {
+    console.log("api error", error)
+  }
+
+  return result;
+}
+
 export async function getDiscussion(id: string): Promise<TDiscussion[] | undefined> {
   let result: TDiscussion[] = [];
   try {
-    const res = await fetch(`http://localhost:3000/api/discussion/search?id=${id}`)
+    const res = await fetch(`${server}/discussion/search?id=${id}`)
     if (!res.ok) return result;
 
     result = await res.json();
@@ -21,27 +39,13 @@ export async function getDiscussion(id: string): Promise<TDiscussion[] | undefin
 export async function getComments(): Promise<TComment[] | undefined> {
   let result: TComment[] = [];
   try {
-    const res = await fetch("http://localhost:3000/api/comments");
+    const res = await fetch(`${server}/comments`);
     if (!res.ok) return result;
 
     result = await res.json();
 
   } catch (error) {
     console.log("api error", error);
-  }
-
-  return result;
-}
-
-export async function getDiscussions(): Promise<TDiscussion[] | undefined> {
-  let result: TDiscussion[] = [];
-  try {
-    const res = await fetch("http://localhost:3000/api/discussions");
-    if (!res.ok) return result;
-
-    result = await res.json();
-  } catch (error) {
-    console.log("api error", error)
   }
 
   return result;
